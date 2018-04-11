080e83ee74a7100ff306eb36017d1e49c0072471
abizer09 <37827975+abizer09@users.noreply.github.com>
2018-04-11 22:49:52 +0530

    Create inputFailure.js

---
+++ inputFailure.js
@@ -0,0 +1,15 @@
+var citizenship = parseInt(prompt("Which countrys' citizenship do you hold?"));
+
+var americanCitizenship = parseInt(prompt("Do you hold a greencard from U.S.A?"));
+
+var countriesAllowed = ["America", "SouthAfrica", "Europe"];
+
+if (citizenship == countriesAllowed, americanCitizenship == Yes) {
+
+	alert("Youre eligable for the visa");
+
+} else {
+
+	alert("Youre not eligable for the visa");
+
+}
